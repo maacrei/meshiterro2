28527fadacd9530d2cbf605c75d25bac6af82710
EC2 Default User <ec2-user@ip-172-31-44-157.ec2.internal>
2022-03-11 09:36:44 +0000

    [Add1]ユーザー一覧ページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "skippr.min"
+import "script.js"
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+import '@fortawesome/fontawesome-free/js/all'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
